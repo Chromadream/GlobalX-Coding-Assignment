3ce8652e95558c9118445a64a4d4701bfb807c74
Jonathan Nicholas <jonicrecis@gmail.com>
2018-12-08 20:56:50 +1100

    Specify early exit, if no file is specified

--- src/GlobalX_Assignment/Program.cs
+++ src/GlobalX_Assignment/Program.cs
@@ -5,8 +5,13 @@ namespace GlobalX_Assignment
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Please specify input file name.");
+                return 1;
+            }
             FileLoader fileLoader = new FileLoader(args[0]);
             FileWriter fileWriter = new FileWriter("./sorted-names-list.txt");
             List<Name> input = Name.BatchConvert(fileLoader.All());
@@ -16,6 +21,7 @@ namespace GlobalX_Assignment
                 Console.WriteLine(item);
                 fileWriter.WriteOne(item.ToString());
             }
+            return 0;
         }
     }
 }
